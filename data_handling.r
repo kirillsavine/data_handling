a34919ab2e72283a682cab57e9d012b7355d1b38
Kirill Savine <kirill.savine@gmail.com>
2018-01-31 14:51:12 -0500

    Update data_handling.r

--- data_handling.r
+++ data_handling.r
@@ -19,3 +19,25 @@ tab=function(x){
 	names(res)=c("vals","na")	
 	res
 }
+
+# performs a left join between 2 data frames on one or more variables. It also provides options for situations where no match was found
+index_match=function(samp,d_samp,o_id_cols_array=c("ag","wkn"),l_id_cols_array=c("ag","wkn"), ret_array=c("weekly_sched_hrs"),ifna=NA) {    
+        samp=as.data.frame(samp)
+        d_samp=as.data.frame(d_samp)
+
+        if(length(o_id_cols_array)>1 | length(l_id_cols_array)>1){
+                res=d_samp[,ret_array][match(do.call(paste0, samp[,o_id_cols_array]), do.call(paste0, d_samp[,l_id_cols_array]))]
+        }else{
+                res=d_samp[,ret_array][match(samp[,o_id_cols_array], d_samp[,l_id_cols_array])] 
+        }
+
+        if(is.na(ifna)){return(res)}    # if ifna is NA: return NA
+        if(ifna=="original"){               
+            res[is.na(res)]=samp[is.na(res),o_id_cols_array] # if ifna=="original": return original value
+        }else{
+            res[is.na(res)]=ifna    # else: return value supplied in the `ifna` arg
+        }   
+
+        return(res)
+
+}
